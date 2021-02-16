7a78f4f9e27b8f2e7835025610c7ac643c505362
ATJUstudent <72507912+ATJUstudent@users.noreply.github.com>
2021-02-16 20:38:00 +0800

    first commit

---
+++ src/renderer/components/login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <el-form ref="loginFormRef" :model="loginform" :rules="login_rules" label-width="80px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginform.username" placeholder="请输入用户名" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="密码">
+          <el-input v-model="loginform.password" placeholder="请输入密码" prefix-icon="el-icon-lock" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="button_groups">
+          <el-button type="primary" @click="login_load">登录</el-button>
+          <el-button type="info">注册</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      loginform: {
+        username: '',
+        password: ''
+      },
+      //表单验证规则
+      login_rules:{
+        username:[
+          { required: true, message: "用户名不能为空",trigger:"blur"},
+          { min: 3, max: 10, message: "长度在3到10个字符之间",trigger:"blur"}
+        ],
+        password:[
+          { required: true, message: "密码不能为空",trigger:"blur"},
+          { min: 5, max: 16, message: "长度在5到16个字符之间",trigger:"blur"}
+        ]
+      }
+    }
+  },
+  methods: {
+    login_load() {
+      this.$refs.loginFormRef.validate(async (valid)=>{
+        console.log(valid);
+        if(!valid) return;
+        //下面需要向服务器发起请求
+        //const {data: res} =await this.$http.post("login",this.loginform);
+        //if (res.meta.status !== 200) return this.$message.error('登录失败');
+        this.$message.success('登录成功');
+      });
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top:  50%;
+  transform: translate(-50%, -50%);
+}
+
+.button_groups {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 20px 20px;
+  box-sizing: border-box;
+}
+</style>
+
+
---
+++ src/renderer/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+//导入样式表
+import './styles/global.css'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+axios.defaults.baseURL = 'http://127.0.0.1:8899'
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
